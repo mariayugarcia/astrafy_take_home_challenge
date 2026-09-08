1bbb51b147518b95fd421ef4ed8c40a2fe6e01b5
María Yu García Muñoz <mariayugarciamnz@gmail.com>
2026-09-08 18:56:59 +0200

    Update exercise_5.sql

--- analyses/exercise_5.sql
+++ analyses/exercise_5.sql
@@ -1,16 +1,12 @@
 SELECT
     order_id,
-    client_id,
+    customer_id,
     order_date,
     previous_orders_12m,
 
     CASE
-        WHEN previous_orders_12m = 0
-            THEN 'New'
-
-        WHEN previous_orders_12m BETWEEN 1 AND 3
-            THEN 'Returning'
-
+        WHEN previous_orders_12m = 0 THEN 'New'
+        WHEN previous_orders_12m BETWEEN 1 AND 3 THEN 'Returning'
         ELSE 'VIP'
     END AS order_segmentation
 
