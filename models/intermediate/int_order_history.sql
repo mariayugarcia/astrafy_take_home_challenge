4d13564e12418513f5c7754471e98cb2ad43f9c6
María Yu García Muñoz <mariayugarciamnz@gmail.com>
2026-09-08 14:00:25 +0200

    Update int_order_history.sql

--- models/intermediate/int_order_history.sql
+++ models/intermediate/int_order_history.sql
@@ -1,13 +1,13 @@
 SELECT
     o.order_id,
-    o.client_id,
+    o.customer_id,
     o.order_date,
     COUNT(previous.order_id) AS previous_orders_12m
 
 FROM {{ ref('stg_orders') }} o
 
 LEFT JOIN {{ ref('stg_orders') }} previous
-    ON o.client_id = previous.client_id
+    ON o.customer_id = previous.customer_id
     AND previous.order_date < o.order_date
     AND previous.order_date >= DATE_SUB(
         o.order_date,
@@ -16,5 +16,5 @@ LEFT JOIN {{ ref('stg_orders') }} previous
 
 GROUP BY
     o.order_id,
-    o.client_id,
+    o.customer_id,
     o.order_date
