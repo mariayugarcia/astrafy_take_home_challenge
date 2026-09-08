f3445e58c2a30e693b989ec80f70df87050db543
María Yu García Muñoz <mariayugarciamnz@gmail.com>
2026-09-08 13:56:45 +0200

    Parse date format for order_date in SQL query

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -3,7 +3,7 @@ WITH source AS (
     SELECT
         orders_id AS order_id,
         customers_id AS customer_id,
-        date_date AS order_date,
+        PARSE_DATE('%d/%m/%Y', date_date) AS order_date,
         net_sales
     FROM {{ source('raw', 'orders_recrutement') }}
 
